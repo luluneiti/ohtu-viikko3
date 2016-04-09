0c3a1d943dbda1361f9363a12379de2aea794389
Ulla G <luluneiti@hotmail.com>
2016-04-09 21:48:36 +0300

    paiv

---
+++ src/main/java/ohtu/App.java
@@ -0,0 +1,82 @@
+package ohtu;
+
+import ohtu.data_access.InMemoryUserDao;
+import ohtu.data_access.UserDao;
+import ohtu.io.ConsoleIO;
+import ohtu.io.IO;
+import ohtu.services.AuthenticationService;
+
+public class App {
+
+    private IO io;
+    private AuthenticationService auth;
+
+    public App(IO io, AuthenticationService auth) {
+        this.io = io;
+        this.auth = auth;
+    }
+
+    public String[] ask() {
+        String[] userPwd = new String[2];
+        userPwd[0] = io.readLine("username:");
+        userPwd[1] = io.readLine("password:");
+        return userPwd;
+    }
+
+    public void run() {
+        while (true) {
+            String command = io.readLine(">");
+            if (command.isEmpty()) {
+                break;
+            }
+            if (command.equals("new") || command.equals("login")) {
+                {
+                    check(command);
+                }
+            }
+        }
+
+    }
+
+    public void check(String command) {
+        String[] usernameAndPasword = ask();
+        if (command.equals("new")) {
+            luodaan(usernameAndPasword);
+        }
+        if (command.equals("login")) {
+            loggaa(usernameAndPasword);
+        }
+    }
+
+    public void luodaan(String[] usernameAndPasword) {
+        if (auth.createUser(usernameAndPasword[0], usernameAndPasword[1])) {
+            io.print("new user registered");
+        } else {
+            io.print("new user not registered");
+        }
+    }
+
+    public void loggaa(String[] usernameAndPasword) {
+        if (auth.logIn(usernameAndPasword[0], usernameAndPasword[1])) {
+            io.print("logged in");
+        } else {
+            io.print("wrong username or password");
+        }
+    }
+
+    public static void main(String[] args) {
+        UserDao dao = new InMemoryUserDao();
+        IO io = new ConsoleIO();
+        AuthenticationService auth = new AuthenticationService(dao);
+        new App(io, auth).run();
+    }
+
+    // testejä debugatessa saattaa olla hyödyllistä testata ohjelman ajamista
+    // samoin kuin testi tekee, eli injektoimalla käyttäjän syötteen StubIO:n avulla
+    //
+    // UserDao dao = new InMemoryUserDao();  
+    // StubIO io = new StubIO("new", "eero", "sala1nen" );   
+    //  AuthenticationService auth = new AuthenticationService(dao);
+    // new App(io, auth).run();
+    // System.out.println(io.getPrints());
+}
